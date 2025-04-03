5083038a43d34534783a297e2a52ebbe620bf348
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-04-03 10:49:52 -0300

    First Commit

---
+++ src/main/java/com/studies/container_tools/ContainerToolsApplication.java
@@ -0,0 +1,13 @@
+package com.studies.container_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studies/container_tools/ContainerToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.container_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
